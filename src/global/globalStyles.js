0c24f7da20e8884ec5fce7bf8d1a7e70d762445f
Adriano <adrianogotardo.12@gmail.com>
2023-02-23 11:23:02 -0300

    changed font importation

--- src/global/globalStyles.js
+++ src/global/globalStyles.js
@@ -5,8 +5,12 @@ const GlobalStyles = createGlobalStyle`
     user-select: none;
     box-sizing: border-box;
   }
+  @font-face {
+    font-family: Roboto;
+    src: url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  }
   body {
-    font-family: "Roboto", sans-serif;
+    font-family: Roboto;
   }
 `;
 
